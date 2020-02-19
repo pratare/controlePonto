bcf7a2fd589dee8e1ab1184b3c45e23d26436376
Renato <renatoprata@gmail.com>
2020-02-18 21:00:25 -0300

    ajuste de metodo

--- src/main/java/com/itau/controlePonto/repository/PontoRepository.java
+++ src/main/java/com/itau/controlePonto/repository/PontoRepository.java
@@ -1,7 +1,6 @@
 package com.itau.controlePonto.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,8 +8,6 @@ import com.itau.controlePonto.models.Ponto;
 import com.itau.controlePonto.models.Usuario;
 
 public interface PontoRepository extends JpaRepository<Ponto, Integer>{
-	
-//	public Optional<Ponto> findByUsuario(Usuario usuario);
 	
 	public List<Ponto> findByUsuario(Usuario usuario);
 
